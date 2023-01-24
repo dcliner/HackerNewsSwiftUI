e5d44dba83e39ab95f07b11bfff9e90b13914325
derefaacline <cderefaa@gmail.com>
2023-01-24 12:03:04 -0600

    Initial Commit

---
+++ HackerNewsSwiftUI/HackerNewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsSwiftUIApp.swift
+//  HackerNewsSwiftUI
+//
+//  Created by Derefaa Cline on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
